eb1070186b71abacc05b251b7969408e31776276
elvisbrevi <elvisbrevi@gmail.com>
2023-08-21 09:23:44 -0400

    passing variables course

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
 fn main() {
-    //todo!  creation
+    // creation
     let a: i16 = 5;
+    // mutability
+    let mut b: i16 = 5;
+    b = 4;
+    // shadowing
+    let c: i16 = 110;
+    let c: i16 = 130;
+
+    println!("c is: {}", c);
+
+    // scope
+    let d: i16 = 5;
+    {
+        let d: i16 = d + 6;
+        println!("d is: {}", d);
+    }
+
+    println!("d is: {}", d);
 }
