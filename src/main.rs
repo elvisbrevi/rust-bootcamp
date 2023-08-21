31a5e4019ef12867b3935df6dfdf581e2ace2441
elvisbrevi <elvisbrevi@gmail.com>
2023-08-21 12:38:39 -0400

    solved functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
+const MAX_PLAYERS: u8 = 10;
+static CASINO_NAME: &str = "Casino Royale";
+
 fn main() {
+    /* CONSTANTS */
+    // two space of memory
+    let a: u8 = MAX_PLAYERS;
+    let a: u8 = MAX_PLAYERS;
+    // one space of memory
+    let b: &str = CASINO_NAME;
+    let b: &str = CASINO_NAME;
+
+    /* FUNCTIONS */
+    let my_value = my_function(22);
+    println!("my function returned: {}", my_value);
+    //variables();
+}
+
+fn my_function(x: i32) -> i32 {
+    println!("my function called with: {}", x);
+    x + 22
+}
+
+// function variables
+fn variables() {
     // creation
     let a: i16 = 5;
     // mutability
