2c88807bd181b299a881fcfb1f41f35a62437962
elvisbrevi <elvisbrevi@gmail.com>
2023-08-25 10:21:53 -0400

    How Rust Achieves Memory Safety

--- src/main.rs
+++ src/main.rs
@@ -76,9 +76,59 @@ fn main() {
     for element in a {
         println!("the value is: {}", element);
     }
+
+    // Ownership
+    let s1 = String::from("hello"); // heap allocated string value
+                                    // let s2 = s1; // s1 is moved to s2
+                                    // print!("s2 is: {}", s2); // s1 is dropped before main function ends
+
+    function_ownwership(s1); // s1 is moved to function
+                             //print!("s2 is: {}", s1);
+
+    // cloned by default for primitive types (copy trait)
+    let x = 5;
+    let y = x;
+    println!("y is: {}", x);
+
+    // solution: clone non-primitive types
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    println!("s2 is: {}", s2);
+
+    // solution: reference
+    let s1 = String::from("hello");
+    let s2 = &s1;
+    println!("s2 is: {}", s2);
+
+    let mut y = 30;
+    let x = &mut y;
+    println!("x and y: {}", x);
+    *x = 40;
+    println!("y and y: {}", x);
+
+    // slices
+    let tweet = String::from("This is a tweet message");
+    let trimmed_tweet = &tweet[..10]; // string slice
+    println!("trimmed tweet: {}", trimmed_tweet);
+    println!("trimmed tweet: {}", trim_tweet(&tweet));
+
+    let tweet2 = "other tweet but literal string";
+    println!("trimmed tweet: {}", trim_tweet(&tweet2));
+
+    let a = [1, 2, 3, 4, 5];
+    let a_slice: &[i32] = &a[1..3];
+    println!("a slice: {:?}", a_slice);
 }
+
+fn function_ownwership(s: String) {
+    println!("s is: {}", s);
+} // s is dropped here
 
 fn my_function(x: i32) -> i32 {
     println!("my function called with: {}", x);
     x + 22
+}
+
+fn trim_tweet(tweet: &str) -> &str {
+    &tweet[..10]
 }
