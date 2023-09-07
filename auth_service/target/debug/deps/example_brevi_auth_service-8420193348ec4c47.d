87e9db24ecf487a53baa7032191375059169ab73
elvisbrevi <elvisbrevi@gmail.com>
2023-09-07 18:09:49 -0300

    updating name lib

---
+++ auth_service/target/debug/deps/example_brevi_auth_service-8420193348ec4c47.d
@@ -0,0 +1,5 @@
+/Users/elvisbrevi/Projects/rust-bootcamp/auth_service/target/debug/deps/example_brevi_auth_service-8420193348ec4c47.rmeta: src/main.rs
+
+/Users/elvisbrevi/Projects/rust-bootcamp/auth_service/target/debug/deps/example_brevi_auth_service-8420193348ec4c47.d: src/main.rs
+
+src/main.rs:
