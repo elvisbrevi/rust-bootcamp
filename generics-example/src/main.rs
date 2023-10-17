9915aa6fc1d7d2af4387bb5403ff4eb966507153
elvisbrevi <elvisbrevi@gmail.com>
2023-10-17 12:16:34 -0300

    finishing generics class

--- generics-example/src/main.rs
+++ generics-example/src/main.rs
@@ -10,16 +10,35 @@ impl<T> BrowserCommand<T> {
             payload,
         }
     }
+
+    fn get_payload(&self) -> &T {
+        &self.payload
+    }
+}
+
+impl BrowserCommand<String> {
+    fn print_payload(&self) {
+        println!("{}", self.payload);
+    }
 }
 
 fn main() {
-    let cmd1 = BrowserCommand {
-        name: "navigate".to_string(), // or String::from("navigate"),
-        payload: String::from("https://www.rust-lang.org"),
-    };
+    let cmd1 = BrowserCommand::new(
+        "navigate".to_string(),
+        String::from("https://www.rust-lang.org"),
+    );
 
-    let cmd2 = BrowserCommand {
-        name: "zoom".to_string(),
-        payload: 200,
-    };
+    let cmd2 = BrowserCommand::new("zoom".to_string(), 200);
+
+    cmd1.print_payload();
+
+    let p1 = cmd1.get_payload();
+    let p2 = cmd2.get_payload();
+
+    serialize_payload(p1);
+    serialize_payload(p2);
+}
+
+fn serialize_payload<T>(payload: T) -> String {
+    "placeholder".to_owned()
 }
