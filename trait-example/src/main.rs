783f162e062bb6fb12aaa4d41c4e640de99b4e03
elvisbrevi <elvisbrevi@gmail.com>
2023-10-23 06:28:21 -0300

    adding traits bounds example

--- trait-example/src/main.rs
+++ trait-example/src/main.rs
@@ -53,5 +53,49 @@ impl Paint for House {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let car = Car {
+        info: VehiculeInfo {
+            make: "Toyota".to_string(),
+            model: "Camry".to_string(),
+            year: 2020,
+        },
+    };
+    let house = House {};
+    let object = create_paintable_object();
+
+    paint_red1(&car);
+    paint_red1(&house);
+    paint_red1(&object);
+
+    paint_vehicule(&car);
+    //paint_vehicule(&house); // error because house dont implement Park
+    //paint_vehicule(&object); // error because object dont implement Park
+}
+
+// Traits bounds
+
+fn paint_red1<T: Paint>(obj: &T) {
+    obj.paint("red".to_string())
+}
+
+fn paint_red2(obj: &impl Paint) {
+    obj.paint("red".to_string())
+}
+
+fn paint_red3<T>(obj: &T)
+where
+    T: Paint,
+{
+    obj.paint("red".to_string())
+}
+
+fn paint_vehicule<T>(obj: &T)
+where
+    T: Paint + Park,
+{
+    obj.paint("red".to_string())
+}
+
+fn create_paintable_object() -> impl Paint {
+    House {}
 }
