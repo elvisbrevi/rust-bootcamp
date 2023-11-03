6b978d50829f69584201f248f73e5fbd27c055de
Elvis Brevi <elvis.brevi@sag.gob.cl>
2023-11-03 06:09:13 -0300

    making example

--- lifetime-elision-rules/src/main.rs
+++ lifetime-elision-rules/src/main.rs
@@ -2,6 +2,10 @@ struct Tweet {
     content: String,
 }
 
+impl Tweet {
+    fn replace_content(&mut self, content: &str) -> &str {}
+}
+
 fn main() {
     let tweet = Tweet {
         content: "example".to_owned(),
