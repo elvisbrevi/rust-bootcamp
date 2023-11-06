43e560245232844a511c6c1b0d8fad20861daccb
elvisbrevi <elvisbrevi@gmail.com>
2023-11-06 07:13:05 -0300

    adding Box Smart Pointer example

--- box-smart-pointer/src/main.rs
+++ box-smart-pointer/src/main.rs
@@ -8,18 +8,29 @@ struct Button {
     text: String,
 }
 
+// case 2: recursive property
+struct Container {
+    name: String,
+    child: Box<Container>,
+}
+
 impl UIComponent for Button {}
+impl UIComponent for Container {}
 
 fn main() {
     // stored in stack
     let button_a = Button {
-        text: "Click me".to_string(),
+        text: "Click me".to_owned(),
     };
 
     // stored in heap
     let button_b = Box::new(Button {
-        text: "Click me".to_string(),
-    }
+        text: "Click me".to_owned(),
+    });
 
-    button_a.render();
+    let button_c = button_a;
+    let button_d = button_b;
+
+    // case 1: specify type
+    let components: Vec<Box<dyn UIComponent>> = vec![Box::new(button_c), button_d];
 }
